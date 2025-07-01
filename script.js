36e04731ab8d92544d0a764374553c59291fd469
Shivam Dev <devsd98@gmail.com>
2025-07-01 19:45:19 +0530

    Project-page-updation

--- script.js
+++ script.js
@@ -21,4 +21,56 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.getElementById('header1').addEventListener('click', () => toggleProject('body1'));
+document.getElementById('header2').addEventListener('click', () => toggleProject('body2'));
+document.getElementById('header3').addEventListener('click', () => toggleProject('body3'));
+
+function toggleProject(bodyId) {
+  const body = document.getElementById(bodyId);
+
+  if (body.style.display === 'flex') {
+     gsap.to(body, {
+      height: 0,
+      opacity: 0,
+      duration: 0.4,
+      ease: "power2.in",
+      onComplete: () => {
+        body.style.display = 'none';
+      }
+    });
+  }else{
+    body.style.display = 'flex'; 
+  gsap.fromTo(body,
+    { height: 0, opacity: 0 },
+    {
+      height: body.scrollHeight,
+      opacity: 1,
+      duration: 0.4,
+      ease: "power2.out",
+      onComplete: () => {
+        body.style.height = 'auto'; 
+      }
+    }
+  );
+  }
+}
+
+function toggleclose() {
+  const arrow = event.currentTarget;
+  const body = arrow.closest('.project-body-card');
+
+  if (body) {
+    gsap.to(body, {
+      height: 0,
+      opacity: 0,
+      duration: 0.4,
+      ease: "power2.in",
+      onComplete: () => {
+        body.style.display = 'none';
+      }
+    });
+  }
+}
+
+
 
